702494aa047e5014677cb5a1f2c6d6045233022e
deciacco <ec@deciacco.com>
2022-06-10 13:11:36 -0500

    beautiful soup

--- Python201/ext_beausoup.py
+++ Python201/ext_beausoup.py
@@ -1,7 +1,14 @@
 from urllib import request
 import requests
+from bs4 import BeautifulSoup
 
 page =  requests.get("https://247ctf.com/scoreboard")
+soup = BeautifulSoup(page.content, "html.parser")
 
-print(page.text)
+table = soup.find("table")
+tbody = table.find("tbody")
+rows = tbody.find_all("tr")
 
+for row in rows:
+    cols = [x.text.strip() for x in row.find_all("td")]
+    print(cols[0], cols[2], cols[4])
